54f7c01a06135f61f29d0caa59314853dfa0cf37
Shwetanshu13 <shwetanshusinha13@gmail.com>
2024-04-03 11:22:17 +0530

    CHanged submit btn name to preview

--- src/components/NewsForm.jsx
+++ src/components/NewsForm.jsx
@@ -99,7 +99,7 @@ const NewsForm = () => {
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="submit"
           >
-            Submit
+            Preview
           </button>
         </div>
       </form>
